b6d74e16d2fea80f33ed71e4dd5155323b8d7fe7
Idris Adebisi <idris.adebisi@yahoo.com>
2023-09-21 08:11:03 +0100

    UPDATE

--- src/event_generator.py
+++ src/event_generator.py
@@ -59,76 +59,40 @@ def normalized_bytes(lag_time, max_bytes):
     normalized_byte = random_byte * lag_time
     return min(normalized_byte, max_bytes)
 
-def generate_netlog(user, dest, avg_time_between_requests, max_bytes):
-    while True:
-        start_time = datetime.now()
+def publish_burst(publisher, topic_path, burst):
+    for event_dict in burst:
+        json_str = json.dumps(event_dict)
+        data = json_str.encode('utf-8')
+        publisher.publish(topic_path, data=data)
+        
+def generate_event(start_time, user, dest, avg_time_between_requests, max_bytes):
         time_diff = random.uniform(0, avg_time_between_requests * 2)
         end_time = start_time + timedelta(seconds=time_diff)
+        return NetLogRaw(
+            user.subcriber_id,
+            user.src_ip,
+            user.src_port,
+            dest.dest_ip,
+            dest.dest_port, 
+            normalized_bytes(time_diff, max_bytes), 
+            normalized_bytes(time_diff, max_bytes), 
+            random.choice(TCP_FLAGS), 
+            start_time.strftime(TIME_FORMAT), 
+            end_time.strftime(TIME_FORMAT), 
+            dest.protocol_name, 
+            dest.protocol_num            
+        )
         
-
-    start_time = datetime.now()
-    time_diff = random.uniform(0, avg_time_between_requests * 2)
-    end_time = start_time + timedelta(seconds=time_diff)
-    
-    return NetLogRaw(user.subcriber_id,
-                     user.src_ip,
-                     user.src_port,
-                     dest.dest_ip,
-                     dest.dest_port, 
-                     normalized_bytes(time_diff, max_bytes), 
-                     normalized_bytes(time_diff, max_bytes), 
-                     random.choice(TCP_FLAGS), 
-                     start_time.strftime(TIME_FORMAT), 
-                     end_time.strftime(TIME_FORMAT), 
-                     dest.protocol_name, 
-                     dest.protocol_num)
-    
-    
-    
-    
-# def sleep_then_publish(publisher, topic_path, data, timestamp):
-#     time.sleep(timestamp)
-#     publisher.publish(topic_path, data=data, timestamp=timestamp)
-    
-# def publish_burst(publisher, topic_path, burst):
-#     for event_dict in burst:
-#         json_str = json.dumps(event_dict)
-#         data = json_str.encode('utf-8')
-#         publisher.publish(topic_path, data=data, timestamp=datetime.now().strftime(TIME_FORMAT))
-
-# def generate_netlog(user, dest):
-#     # src_port, dest_port, protocol_name, protocol_num, max_byte
-#     publisher = pubsub_v1.PublisherClient()
-#     topic_path = publisher.topic_path(project_id, topic_name)
-#     events = []
-#     while True:
-#         # source , destination = 
-#         start_time = datetime.now()
-#         time_diff = random.uniform(0, avg_time_between_requests * 2)
-#         end_time = start_time + timedelta(seconds=time_diff)
-        
-#         yield NetLogRaw(user.subcriber_id,
-#                         user.src_ip,
-#                         user.src_port,
-#                         dest.dest_ip,
-#                         dest.dest_port, 
-#                         normalized_bytes(time_diff, max_bytes), 
-#                         normalized_bytes(time_diff, max_bytes), 
-#                         random.choice(TCP_FLAGS), 
-#                         start_time.strftime(TIME_FORMAT), 
-#                         end_time.strftime(TIME_FORMAT), 
-#                         dest.protocol_name, 
-#                         dest.protocol_num)
- 
-# def burst_generator(num_events, users, dest_ips, avg_time_between_requests, max_bytes):
-#     for event_dict in burst:
-#         json_str = json.dumps(event_dict)
-#         data = json_str.encode('utf-8')
-#         publisher.publish(topic_path, data=data, timestamp=event_dict['timestamp'])
-        
-        
-    for _ in range(num_events):
-        yield generate_netlog(users, dest_ips, avg_time_between_requests, max_bytes)        
+def create_process(user, dest, avg_time_between_requests, max_bytes, max_events_per_burst):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_name)
+    num_events = random.randint(0, max_events_per_burst)
+    burst = []
+    while len(burst) < num_events:
+        start_time = datetime.now() if len(burst) == 0 else burst[-1]['endTime'] 
+        burst.append(generate_event(start_time, user, dest, avg_time_between_requests, max_bytes))
+    publish_burst(publisher, topic_path, burst)
+     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(__file__, description="Netlog Data Generator")
     parser.add_argument("--num_events", "-e", type=int, dest="num_events", help="The number of events", default=1000000)
